cc7e3edf1888503a26edc7851a6de516af57e259
Willem Verbuyst <willemverbuyst@gmail.com>
2022-11-23 04:15:02 +0100

    test: add test for prepend method

--- src/linkedList.spec.ts
+++ src/linkedList.spec.ts
@@ -70,3 +70,28 @@ describe('LinkedList pop method', () => {
     expect(instance.length).toBe(0)
   })
 })
+
+describe('LinkedList prepend method', () => {
+  const instance = new LinkedList('z')
+
+  it('should prepend when list is empty', () => {
+    instance.pop()
+    instance.prepend('a')
+    expect(instance.head?.value).toBe('a')
+    expect(instance.tail?.value).toBe('a')
+    expect(instance.length).toBe(1)
+  })
+
+  it.each([
+    ['b', 2],
+    ['c', 3],
+    ['d', 4],
+    ['e', 5],
+    ['f', 6],
+  ])("should prepend '%s'", (value, expected) => {
+    instance.prepend(value)
+    expect(instance.head?.value).toBe(value)
+    expect(instance.tail?.value).toBe('a')
+    expect(instance.length).toBe(expected)
+  })
+})
