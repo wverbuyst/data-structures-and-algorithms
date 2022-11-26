31a21e9d0e8a40e48c54f8f9e9c730461a9bf16e
Willem Verbuyst <willemverbuyst@gmail.com>
2022-11-26 04:35:07 +0100

    feat: add DoublyLinkedList class

--- src/doublyLinkedList.ts
+++ src/doublyLinkedList.ts
@@ -6,3 +6,31 @@ export class Node {
     this.next = null
   }
 }
+
+export class DoublyLinkedList {
+  head: Node | null
+  tail: Node | null
+  length: number
+  constructor(public value: unknown) {
+    const newNode = new Node(value)
+    this.head = newNode
+    this.tail = newNode
+    this.length = 1
+  }
+
+  printElements(): void {
+    let temp = this.head
+    let i = 0
+    while (i < this.length) {
+      // eslint-disable-next-line no-console
+      console.log(`Element ${i + 1}: ${temp?.value}`)
+      temp = temp?.next || null
+      i++
+    }
+  }
+
+  printList(): void {
+    // eslint-disable-next-line no-console
+    console.dir(this, { depth: null })
+  }
+}
