dad3afc068b1a6cac4299b21ad5cf0541bb6713a
Willem Verbuyst <willemverbuyst@gmail.com>
2022-11-28 04:23:31 +0100

    feat: add prepend method

--- src/doublyLinkedList.spec.ts
+++ src/doublyLinkedList.spec.ts
@@ -114,3 +114,30 @@ describe('DoublyLinkedList pop method', () => {
     expect(instance.length).toBe(0)
   })
 })
+
+describe('DoublyLinkedList prepend method', () => {
+  const instance = new DoublyLinkedList('z')
+
+  it('should prepend when list is empty', () => {
+    instance.pop()
+    instance.prepend('a')
+
+    expect(instance.head?.value).toBe('a')
+    expect(instance.tail?.value).toBe('a')
+    expect(instance.length).toBe(1)
+  })
+
+  it.each([
+    ['b', 2],
+    ['c', 3],
+    ['d', 4],
+    ['e', 5],
+    ['f', 6],
+  ])("should prepend '%s'", (value, expected) => {
+    instance.prepend(value)
+
+    expect(instance.head?.value).toBe(value)
+    expect(instance.tail?.value).toBe('a')
+    expect(instance.length).toBe(expected)
+  })
+})
--- src/doublyLinkedList.ts
+++ src/doublyLinkedList.ts
@@ -64,4 +64,18 @@ export class DoublyLinkedList {
     this.length--
     return temp
   }
+
+  prepend(value: unknown) {
+    const newNode = new Node(value)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      newNode.next = this.head
+      this.head.prev = newNode
+      this.head = newNode
+    }
+    this.length++
+    return true
+  }
 }
